d572d373c765a2cfead9fd2edb6b7c48d960f1bb
sorenchu <antonioostras@gmail.com>
2020-05-14 09:16:03 +0200

    making main activity to show through cli data

--- app/src/main/java/com/example/assistancecontrol/MainActivity.java
+++ app/src/main/java/com/example/assistancecontrol/MainActivity.java
@@ -4,11 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import dao.PlayerDao;
+import model.Player;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     public static final String EXTRA_MESSAGE = "com.example.assistancecontrol.MESSAGE";
 
@@ -27,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
                 android.R.layout.simple_spinner_dropdown_item
         );
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
     }
 
     public void sendMessage(View view) {
@@ -35,5 +42,22 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void onItemSelected(AdapterView<?> adapter, View view, int position, long id) {
+        String category = (String) adapter.getItemAtPosition(position);
+        System.out.println("category" + category);
+        ArrayList<Player> players = PlayerDao.getPlayerByCategory(category);
+        if (players == null) {
+            System.out.println("Category different from u18");
+        } else {
+            for (Player player : players) {
+                System.out.println(player.getSurnameAndName());
+            }
+        }
+    }
+
+    public void onNothingSelected(AdapterView<?> adapter) {
+
     }
 }
