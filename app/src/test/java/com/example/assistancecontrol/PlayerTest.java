4c513fb5cb78432afb761ec8c21aff91134504e0
sorenchu <antonioostras@gmail.com>
2020-05-15 19:57:35 +0200

    getting rid of useless test

--- app/src/test/java/com/example/assistancecontrol/PlayerTest.java
+++ app/src/test/java/com/example/assistancecontrol/PlayerTest.java
@@ -2,25 +2,12 @@ package com.example.assistancecontrol;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import model.Category;
 import model.Player;
 
 import static org.junit.Assert.*;
 
 public class PlayerTest {
-
-    @Test
-    public void addPlayerToList() {
-        Player player = new Player();
-        List<Player> playerList = new ArrayList<>();
-        playerList.add(player);
-        assertFalse(playerList.isEmpty());
-        assertEquals(1, playerList.size());
-        assertTrue(playerList.contains(player));
-    }
 
     @Test
     public void getNameAndSurname() {
