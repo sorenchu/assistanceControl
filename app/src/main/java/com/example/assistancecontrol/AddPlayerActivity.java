89164f5f75c908ed375ea474ff4c1f425434c736
sorenchu <antonioostras@gmail.com>
2020-05-15 21:03:01 +0200

    allowing to enter new players in add player activity

--- app/src/main/java/com/example/assistancecontrol/AddPlayerActivity.java
+++ app/src/main/java/com/example/assistancecontrol/AddPlayerActivity.java
@@ -1,11 +1,21 @@
 package com.example.assistancecontrol;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
+import dao.PlayerDao;
+import model.Player;
+
+import static model.Category.*;
+
 public class AddPlayerActivity extends AppCompatActivity {
+
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,13 +24,38 @@ public class AddPlayerActivity extends AppCompatActivity {
         this.showSpinner();
     }
 
+    public void savePlayer(View view) {
+        Intent intent = new Intent(this, AddPlayerActivity.class);
+        Player player = this.generatePlayer();
+        PlayerDao.addPlayerToCategory(player);
+        startActivity(intent);
+        finish();
+    }
+
     private void showSpinner() {
-        Spinner spinner = (Spinner) findViewById(R.id.select_category_spinner);
+        this.spinner = (Spinner) findViewById(R.id.select_category_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this,
                 R.array.category_array,
                 android.R.layout.simple_spinner_dropdown_item
         );
-        spinner.setAdapter(adapter);
+        this.spinner.setAdapter(adapter);
+    }
+
+    // TODO: should this be here?
+    private Player generatePlayer() {
+        EditText editText = (EditText) findViewById(R.id.editText3);
+        String name = editText.getText().toString();
+        editText = (EditText) findViewById(R.id.editText4);
+        String surname = editText.getText().toString();
+        String category = this.spinner.getSelectedItem().toString();
+        switch (category) {
+            case "Sub18":
+                return new Player(name, surname, U18);
+            case "Sub16":
+                return new Player(name, surname, U16);
+            default:
+                return null;
+        }
     }
 }
